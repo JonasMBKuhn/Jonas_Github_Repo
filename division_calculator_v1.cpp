6fcfcc6335ccf6473d91a36c005a65bc3367ee0a
Jonas Kuhn <jonasflames@outlook.com>
2023-05-11 18:50:54 -0500

    implemented potential fix for formatting issue with comments

--- division_calculator_v1.cpp
+++ division_calculator_v1.cpp
@@ -5,51 +5,51 @@
 
 int main()
 {
-	int dividend{ 0 };						 //obtaining dividend
+	int dividend{ 0 };                       //obtaining dividend
 	std::cout << "Please enter a dividend: ";//}
-	std::cin >> dividend;					 //}
+	std::cin >> dividend;                    //}
 
 
 
-	int divisor{ 0 };									 //obtaining non-zero divisor
-	while (divisor == 0) {								 //}(as dividing by zero is... well, impossible)
+	int divisor{ 0 };                                    //obtaining non-zero divisor
+	while (divisor == 0) {                               //}(as dividing by zero is... well, impossible)
 		std::cout << "Please enter a non-zero divisor: ";//}
-		std::cin >> divisor;							 //}
-	}													 //}
+		std::cin >> divisor;                             //}
+	}                                                    //}
 
-	int significant_digits{ 0 };																			 //obtaining the number of significant digits
-	while (significant_digits < 1) {																		 //}(to us this means the # of chars (in this case digits of the quotient) the program outputs before closing)
-		std::cout << "Please enter the desired number of significant digits\n" <<							 //}the first time it is used this will only act as a baseline that generates too many digits (wich will be truncated befor output to console) 
+	int significant_digits{ 0 };                                                                             //obtaining the number of significant digits
+	while (significant_digits < 1) {                                                                         //}(to us this means the # of chars (in this case digits of the quotient) the program outputs before closing)
+		std::cout << "Please enter the desired number of significant digits\n" <<                            //}the first time it is used this will only act as a baseline that generates too many digits (wich will be truncated befor output to console) 
 			"(keep in mind the program cannot run a negative number of times and will not run zero times): ";//}as this is actually more efficient than counting how many digits each calcuulation adds
-		std::cin >> significant_digits;																		 //}(this is in a loop until we get a number grater than zero so the program does not:
-	}																										 //}repeat infinately, close immedeatly, ect. (avoids undefined behavior))
+		std::cin >> significant_digits;                                                                      //}(this is in a loop until we get a number grater than zero so the program does not:
+	}                                                                                                        //}repeat infinately, close immedeatly, ect. (avoids undefined behavior))
 	///////////////////////////////////////////////////////////////////////please note that due to time constraints the ^^^above^^^ is not quite true and the program just ouputs too many chars as I have not yet added string truncation
 	int remainder{ 0 };
 
 
 
 	if (dividend < 0 != divisor < 0) {//making sure that the '-' sign only appears before the entire # and not before every digit
-									  //}(this happens because (practically speaking) every digit gets its own operation)
-		std::cout << '-';			  //}(if the divisor/dividend is negative we have to say that now)
-									  //}we detect the issue by seeing if the dividend OR(exclusively) the divisor is negative (as -#/-# = +#)
-		divisor *= -1;				  //}(to fix this we just make everything positive after this point)
-									  //}
-	}								  //}
+	                                  //}(this happens because (practically speaking) every digit gets its own operation)
+		std::cout << '-';             //}(if the divisor/dividend is negative we have to say that now)
+                                      //}we detect the issue by seeing if the dividend OR(exclusively) the divisor is negative (as -#/-# = +#)
+		divisor *= -1;                //}(to fix this we just make everything positive after this point)
+                                      //}
+	}                                 //}
 
 
 
 	std::cout << dividend / divisor << '.';//getting the '>.0' part out of the way :) (by doing the first cycle outside the loop, then adding a '.')
-	remainder = dividend % divisor;		   //}this also ensures that even with a significant digit of 1 we would at least recieve a good number (technically incorrect)
+	remainder = dividend % divisor;        //}this also ensures that even with a significant digit of 1 we would at least recieve a good number (technically incorrect)
 
 
 
 	for (int count{ 1 }; count < significant_digits && remainder != 0; ++count) {//basically saying: execute the following until we have output the correct # of digits or got a remainder of zero
-																				 //}going one whole # division at a time by:
-		dividend = remainder * 10;												 //}making the new dividend the remainder times 10
-		std::cout << dividend / divisor;										 //}dividing the new 'dividend' by our divisor
-		remainder = dividend % divisor;											 //}...and setting the remainder as the new remainder of the previous steps
-																				 //}
-	}																			 //}
+                                                                                 //}going one whole # division at a time by:
+		dividend = remainder * 10;                                               //}making the new dividend the remainder times 10
+		std::cout << dividend / divisor;                                         //}dividing the new 'dividend' by our divisor
+		remainder = dividend % divisor;                                          //}...and setting the remainder as the new remainder of the previous steps
+                                                                                 //}
+	}                                                                            //}
 }
 //Currently this program prints too many digits;
 //I am aware of how to solve this issue 
--- print_ascii_table_v1.cpp
+++ print_ascii_table_v1.cpp
@@ -17,3 +17,12 @@ int main() //prints an ascii table
 }
 
 //this file and ones like it have v1 in their names only temporarily for complicated reasons related to trouble renaming/deleting files with a failing SSD
+
+////////////////////////////////////////////////////////////////////////////////
+//a wild commit test comment has appeared!                                   ///
+//                                                                           ///
+//-> use commit [type: git bash] *effective*                                 ///
+//__ use copy [type: file explorererer] *not very effective*                 ///
+//__ use disable compiler warnings [type: visual studio] *not very effective*///
+//__ use yell [type: emotional] *no effect*                                  ///
+////////////////////////////////////////////////////////////////////////////////
